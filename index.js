dea0adc1403c92a613055b377a9e3b1af51918d7
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-11 15:19:37 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, stringText) {
+    return drivers.filter((possibleMatch) => possibleMatch.toLowerCase() === stringText.toLowerCase());
+  }
+  
+//   function fuzzyMatch(drivers, stringWithLetters) {
+//     return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(stringWithLetters.toLowerCase()) === 0);
+//   }
+  
+//   function matchName(drivers, driverName) {
+//     return  drivers.filter((record) => record.name === driverName);
+//   }
