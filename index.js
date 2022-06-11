5dde8e7962d9ecb09c7169eb646b89c8993c463f
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-11 15:21:13 +0300

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function findMatching(drivers, stringText) {
     return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(stringWithLetters.toLowerCase()) === 0);
   }
   
-//   function matchName(drivers, driverName) {
-//     return  drivers.filter((record) => record.name === driverName);
-//   }
+  function matchName(drivers, driverName) {
+    return  drivers.filter((record) => record.name === driverName);
+  }
