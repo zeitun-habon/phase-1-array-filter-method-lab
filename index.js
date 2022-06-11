cec6e66675b05a0fa481dd70aaa56778be7ce66e
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-11 15:20:36 +0300

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function findMatching(drivers, stringText) {
     return drivers.filter((possibleMatch) => possibleMatch.toLowerCase() === stringText.toLowerCase());
   }
   
-//   function fuzzyMatch(drivers, stringWithLetters) {
-//     return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(stringWithLetters.toLowerCase()) === 0);
-//   }
+  function fuzzyMatch(drivers, stringWithLetters) {
+    return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(stringWithLetters.toLowerCase()) === 0);
+  }
   
 //   function matchName(drivers, driverName) {
 //     return  drivers.filter((record) => record.name === driverName);
